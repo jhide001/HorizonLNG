713f8aba97c19c06c05c1cd53434b559798e6aa8
Stephen Adesina <jhide001@gmail.com>
2026-06-29 01:32:39 -0500

    HLNG-012: Replace for loops with Java 8 Streams in Main.java

--- src/Main.java
+++ src/Main.java
@@ -9,31 +9,32 @@ public class Main {
         repo.loadDealsFromCSV("deals.csv");
 
         double currentMarketPrice = 140000.00;
-        int processedCount = 0;
-        int skippedCount = 0;
+        int[] processedCount = {0};
+        int[] skippedCount = {0};
 
-        for (EndurDeal deal : repo.getDeals()) {
+        repo.getDeals().stream()
+                .forEach(deal -> {
             try {
                 if (deal.isActive()) {
                     double mtm = deal.calculateMTM(currentMarketPrice);
                     System.out.println("[PROCESSED] " + deal
                             + " | " + DealUtils.formatMTM(mtm));
-                    processedCount++;
+                    processedCount[0]++;
                 } else {
                     System.out.println("[SKIPPED]   " + deal);
-                    skippedCount++;
+                    skippedCount[0]++;
                 }
             } catch (Exception e) {
-                System.out.println("[ERROR] " + e.getMessage());
-            }
-        }
+                System.out.println("[ERROR] " + e.getMessage()); }
+                });
+
 
         System.out.println();
         System.out.println("=== Batch Complete ===");
         System.out.println("Portfolio MTM: "
                 + DealUtils.formatMTM(repo.getTotalMTM(currentMarketPrice)));
-        System.out.println("Processed: " + processedCount);
-        System.out.println("Skipped: " + skippedCount);
+        System.out.println("Processed: " + processedCount[0]);
+        System.out.println("Skipped: " + skippedCount[0]);
         System.out.println("Total: " + repo.getDealCount());
 
         System.out.println();
@@ -47,11 +48,10 @@ public class Main {
 
         System.out.println();
         System.out.println("=== Active Deals Only ===");
-        for (EndurDeal deal : repo.getActiveDeals()) {
-            System.out.println(deal);
-        }
-
+        repo.getActiveDeals().stream()
+                .forEach(deal -> System.out.println(deal));
         System.out.println();
+        System.out.println("=== Shell Trading Deals ===");
         System.out.println("=== Shell Trading Deals ===");
         for (EndurDeal deal : repo.findByCounterparty("Shell Trading")) {
             System.out.println(deal);
